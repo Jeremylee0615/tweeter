bc67c55117131b0edc95410879ce8739fbe45398
Jeremy Lee <jeremylee0615@gmail.com>
2023-05-10 17:51:11 +0900

    Created tweets & organized/edited css designs

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
     const maxText = 140
     const typedText = $(this).val()
     const remainingText = maxText - typedText.length
-    
+
     const $counter = $(".counter")
     $counter.text(remainingText) 
     if (remainingText < 0) {
       $counter.css("color", "Red")
     } else {
-      $counter.css("color", "#F4F1EC")
+      $counter.css("color", "#545149")
     }
   })
 })
