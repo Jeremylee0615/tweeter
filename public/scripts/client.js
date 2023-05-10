aec3fcd024088686be5a38b36a389fc61ff20ffb
Jeremy Lee <jeremylee0615@gmail.com>
2023-05-11 06:32:47 +0900

    added error feature if the tweets are less/more than the words limit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,40 +1,11 @@
 $(document).ready(function() {
-	/*const data = [
-		{
-			"user": {
-				"name": "Newton",
-				"avatars": "https://i.imgur.com/73hZDYK.png",
-				"handle": "@SirIsaac"
-			},
-			"content": {
-				"text": "If I have seen further it is by standing on the shoulders of giants"
-			},
-			"created_at": 1461116232227
-		},
-		{
-			"user": {
-				"name": "Descartes",
-				"avatars": "https://i.imgur.com/nlhLi3I.png",
-				"handle": "@rd"
-			},
-			"content": {
-				"text": "Je pense , donc je suis"
-			},
-			"created_at": 1461113959088
-		}
-	];*/
-
-	const renderTweets = function(tweets) {
-		// loops through tweets
-		// calls createTweetElement for each tweet
-		// takes return value and appends it to the tweets container
-		//console.log("renderTweets")
+	
+  const renderTweets = function(tweets) {
 		for (obj of tweets) {
 			const $tweetElement = createTweetElement(obj);
 			$(".tweet-container").prepend($tweetElement);
 		}
 	};
-
 
 	const createTweetElement = function(tweet) {
 		let $tweet = `
@@ -67,15 +38,21 @@ $(document).ready(function() {
 	$("#tweetForm").submit(function(event) {
 		event.preventDefault();
 		const tweetContent = $("#tweet-text").val();
-		const serialized = $(this).serialize();
-		//console.log("logout", tweetContent)
-		$.ajax({
-			url: "/tweets",
-			data: serialized,
-			type: "POST"
-		}).then(function(response) {
-		//console.log(tweetContent)
-		});
+		if (tweetContent.length === 0) {
+      $(".error").text("Opps, you have not entered anyting.")
+    } else if (tweetContent.length > 140) {
+      $(".error").text("Sorry, You have exceeded the words limit that you can tweet.")
+    } else {
+    const serialized = $(this).serialize();
+		console.log("logout", tweetContent)
+		  $.ajax({
+			  url: "/tweets",
+			  data: serialized,
+			  type: "POST"
+		  }).then(function(response) {
+		console.log(tweetContent)
+		  });
+    }
 	});
   
   const loadTweets = () => {
