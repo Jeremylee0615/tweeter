d09aadde4be0910d63213fbd5e48e7708c40c582
Jeremy Lee <jeremylee0615@gmail.com>
2023-05-11 05:26:38 +0900

    form submission with JQuery & eslinted codes

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-  $("textarea").on("input", function () {
-    const maxText = 140
-    const typedText = $(this).val()
-    const remainingText = maxText - typedText.length
+	$("textarea").on("input", function () {
+		const maxText = 140;
+		const typedText = $(this).val();
+		const remainingText = maxText - typedText.length;
 
-    const $counter = $(".counter")
-    $counter.text(remainingText) 
-    if (remainingText < 0) {
-      $counter.css("color", "Red")
-    } else {
-      $counter.css("color", "#545149")
-    }
-  })
-})
+		const $counter = $(".counter");
+		$counter.text(remainingText); 
+		if (remainingText < 0) {
+			$counter.css("color", "Red");
+		} else {
+			$counter.css("color", "#545149");
+		}
+	});
+});
